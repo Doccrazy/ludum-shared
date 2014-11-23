39b646a0f69fa6a344b5c14ec080b4fa4116ab79
Matthias Piepkorn <mpiepk@gmail.com>
2014-11-23 18:05:47 +0100

    Added Spriter loader + helper classes (see https://github.com/Trixt0r/spriter)

---
+++ src/main/java/de/doccrazy/shared/game/actor/SpriterActor.java
@@ -0,0 +1,40 @@
+package de.doccrazy.shared.game.actor;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.Vector2;
+import com.brashmonkey.spriter.Entity;
+import com.brashmonkey.spriter.Player;
+import de.doccrazy.shared.game.world.Box2dWorld;
+import de.doccrazy.shared.spriter.GdxDrawer;
+
+public abstract class SpriterActor extends ShapeActor {
+    protected final Player player;
+    private final DrawerProvider drawerProvider;
+
+    public SpriterActor(Box2dWorld world, Vector2 spawn, boolean spawnIsLeftBottom, Entity entity, DrawerProvider drawerProvider) {
+        super(world, spawn, spawnIsLeftBottom);
+        this.drawerProvider = drawerProvider;
+        player = new Player(entity);
+        //1 pixel in Spriter <-> 1cm in world coordinates
+        player.setScale(0.01f);
+    }
+
+    @Override
+    protected void doAct(float delta) {
+        super.doAct(delta);
+        player.speed = (int) (delta * 1000);
+        player.update();
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        updatePosition();
+        applyClientTransform(batch, true);
+        drawerProvider.getDrawer().withBatch(batch).draw(player);
+        resetTransform(batch);
+    }
+
+    public interface DrawerProvider {
+        GdxDrawer getDrawer();
+    }
+}
