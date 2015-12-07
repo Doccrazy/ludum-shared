07ce360ddd1e01e7b1001279e3f1ad7c7c6cf1ef
Matthias Piepkorn <mpiepk@gmail.com>
2015-12-07 22:21:58 +0100

    Added blur helper methods

--- src/main/java/de/doccrazy/shared/core/ResourcesBase.java
+++ src/main/java/de/doccrazy/shared/core/ResourcesBase.java
@@ -14,6 +14,7 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.Array;
+import de.doccrazy.shared.game.base.BlurUtils;
 
 public abstract class ResourcesBase {
     protected final TextureAtlas atlas;
@@ -53,6 +54,13 @@ public abstract class ResourcesBase {
 
     protected Texture texture(String filename) {
         return new Texture(Gdx.files.internal(filename));
+    }
+
+    protected Texture textureWrap(String filename) {
+        Texture tex = texture(filename);
+        tex.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
+        tex.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        return tex;
     }
 
     protected Sound sound(String filename) {
@@ -98,4 +106,50 @@ public abstract class ResourcesBase {
     public TextureAtlas getAtlas() {
         return atlas;
     }
+
+    protected Animation[] createBlurLevels(Animation src, int radius, int levels) {
+        Animation[] result = new Animation[levels];
+        result[0] = src;
+        for (int i = 1; i < levels; i++) {
+            result[i] = blur(src, radius, i);
+        }
+        return result;
+    }
+
+    protected Sprite[] createBlurLevels(Sprite src, int levels) {
+        Sprite[] result = new Sprite[levels];
+        result[0] = src;
+        for (int i = 1; i < levels; i++) {
+            result[i] = blur(src, 2, i);
+        }
+        return result;
+    }
+
+    protected Texture blur(Texture src, int radius, int iterations) {
+        src.getTextureData().prepare();
+        Pixmap pixmap = src.getTextureData().consumePixmap();
+        Pixmap blurred = BlurUtils.blur(pixmap, radius, iterations, src.getTextureData().disposePixmap());
+        return new Texture(blurred);
+    }
+
+    protected Sprite blur(Sprite src, int radius, int iterations) {
+        src.getTexture().getTextureData().prepare();
+        Pixmap pixmap = src.getTexture().getTextureData().consumePixmap();
+        Pixmap blurred = BlurUtils.blur(pixmap, src.getRegionX(), src.getRegionY(), src.getRegionWidth(), src.getRegionHeight(),
+                0, 0, src.getRegionWidth(), src.getRegionHeight(), radius, iterations, src.getTexture().getTextureData().disposePixmap());
+        return new Sprite(new Texture(blurred));
+    }
+
+    protected Animation blur(Animation src, int radius, int iterations) {
+        Array<TextureRegion> newFrames = new Array<>();
+        for (TextureRegion frame : src.getKeyFrames()) {
+            frame.getTexture().getTextureData().prepare();
+            Pixmap pixmap = frame.getTexture().getTextureData().consumePixmap();
+            Pixmap blurred = BlurUtils.blur(pixmap, frame.getRegionX(), frame.getRegionY(), frame.getRegionWidth(), frame.getRegionHeight(),
+                    0, 0, frame.getRegionWidth(), frame.getRegionHeight(), radius, iterations, frame.getTexture().getTextureData().disposePixmap());
+            newFrames.add(new TextureRegion(new Texture(blurred)));
+        }
+        return new Animation(src.getFrameDuration(), newFrames, src.getPlayMode());
+    }
+
 }
